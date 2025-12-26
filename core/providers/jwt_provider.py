1164a3ac0868f99818dcf3efface366dc8b45860
eliezer-chaves <chaves.eliezer@outlook.com>
2025-12-26 17:36:42 -0300

    refactor: enhance JWT provider with refresh token generation and hashing functions

--- core/providers/jwt_provider.py
+++ core/providers/jwt_provider.py
@@ -1,10 +1,15 @@
 from datetime import datetime, timedelta, timezone
 from jose import jwt
 import os
+import secrets
+import hmac, hashlib
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS"))
+COOKIE_SECRET = os.getenv("COOKIE_SECRET")
 
 def create_access_token(data: dict):
     to_encode = data.copy()
@@ -20,3 +25,16 @@ def decode_access_token(token: str):
         return payload
     except JWTError:
         return None
+
+def generate_refresh_token() -> str:
+    return secrets.token_urlsafe(64)
+
+def hash_refresh_token(token: str) -> str:
+    return hmac.new(
+        COOKIE_SECRET.encode(),
+        token.encode(),
+        hashlib.sha256
+    ).hexdigest()
+
+def get_refresh_token_expiration() -> datetime:
+    return datetime.now(timezone.utc) + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
